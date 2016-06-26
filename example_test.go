5418ecc69ef882ab1541862bb417a3e4a980b0f6
Carl Kittelberger <icedream@icedream.pw>
2016-06-26 12:02:53 +0200

    Add separate examples for Client.

--- example_test.go
+++ example_test.go
@@ -25,3 +25,25 @@ func Example() {
 		fmt.Println(season.Id, season.Caption)
 	}
 }
+
+func ExampleClient() {
+	// Create client
+	client := footballdata.NewClient(http.DefaultClient)
+
+	/* Do something with the client instance... */
+	seasons, err := client.SoccerSeasons().Do()
+	if err != nil {
+		panic(err)
+	}
+	for _, season := range seasons {
+		fmt.Println(season.Id, season.Caption)
+	}
+}
+
+func ExampleClient_withToken() {
+	// Create client
+	client := footballdata.NewClient(http.DefaultClient)
+
+	// Tell it to use our API token
+	client.SetToken("<insert your api token here>")
+}
